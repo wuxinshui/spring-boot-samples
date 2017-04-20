a68e77fa24eb6e677f3c82f65ffb4dd2df3ffb67
renfuji <renfuji@quarkfinance.com>
2017-04-20 14:17:18 +0800

    设置下载文件名

--- spring-boot-sample-tkmybatis/src/main/java/com/wxs/tkmybatis/controller/DownloadController.java
+++ spring-boot-sample-tkmybatis/src/main/java/com/wxs/tkmybatis/controller/DownloadController.java
@@ -7,7 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import static com.wxs.tkmybatis.util.ExcelUtil.createExcel;
 
@@ -27,9 +27,10 @@ import static com.wxs.tkmybatis.util.ExcelUtil.createExcel;
 @RequestMapping("/download")
 public class DownloadController {
 
-	@RequestMapping(value = "/file/{filename}", method = RequestMethod.GET)
-	public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+	@RequestMapping(value = "/file", method = RequestMethod.GET)
+	public ResponseEntity<Resource> downloadFile() {
 		ByteArrayOutputStream bos = null;
+		String filename = "测试.xlsx";
 		try {
 			Workbook workbook = createExcel();
 			bos = new ByteArrayOutputStream();
@@ -41,7 +42,9 @@ public class DownloadController {
 			headers.add("Pragma", "no-cache");
 			headers.add("Expires", "0");
 			headers.add("charset", "utf-8");
-			headers.add("file", filename);
+			//设置下载文件名
+			filename = URLEncoder.encode(filename, "UTF-8");
+			headers.add("Content-Disposition", "attachment;filename=\"" + filename + "\"");
 
 			Resource resource = new InputStreamResource(new ByteArrayInputStream(bos.toByteArray()));
 
@@ -58,6 +61,4 @@ public class DownloadController {
 		}
 		return null;
 	}
-
-
 }
