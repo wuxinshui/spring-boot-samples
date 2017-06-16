cb0ef8e8e6cb3ec35a24eb7f2bddab05b821ce90
renfuji <renfuji@quarkfinance.com>
2017-06-16 15:39:49 +0800

    add 	@Cacheable(value = "search", keyGenerator = "keyGenerator")

--- spring-boot-sample-redis/src/main/java/com/wxs/redis/conf/RedisCacheConfig.java
+++ spring-boot-sample-redis/src/main/java/com/wxs/redis/conf/RedisCacheConfig.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
 
 /**
  * @Description:
@@ -21,6 +24,24 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @Configuration
 @EnableCaching
 public class RedisCacheConfig extends CachingConfigurerSupport {
+
+	@Bean
+	public KeyGenerator keyGenerator() {
+		return new KeyGenerator() {
+			@Override
+			public Object generate(Object target, Method method, Object... params) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(target.getClass().getName());
+				sb.append(method.getName());
+				for (Object obj : params) {
+					sb.append(obj.toString());
+				}
+				return sb.toString();
+			}
+		};
+	}
+
+
 	@Bean
 	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
 		RedisTemplate<String, String> template = new RedisTemplate<>();
--- spring-boot-sample-redis/src/main/java/com/wxs/redis/controller/UserController.java
+++ spring-boot-sample-redis/src/main/java/com/wxs/redis/controller/UserController.java
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -34,11 +37,36 @@ public class UserController extends BaseResult {
 	private UserService userService;
 
 	@RequestMapping(value = "/search", method = RequestMethod.POST)
-	@Cacheable(value = "search")
+	@Cacheable(value = "search", keyGenerator = "keyGenerator")
 	public ModelMap search() {
 		List<User> userList = userService.selectAll();
 		LOGGER.info("search....");
 		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, userList);
+	}
+
+	@RequestMapping(value = "/selectOne", method = RequestMethod.POST)
+	@Cacheable(value = "search", keyGenerator = "keyGenerator")
+	public ModelMap search(@RequestBody User user) {
+		getUserIds(user);
+		List<User> userList = userService.selectByUsernameAndPassword(user);
+		LOGGER.info("search....");
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, userList);
+	}
+
+	private void getUserIds(@RequestBody User user) {
+		List<Integer> userIds = new ArrayList<>();
+		userIds.add(1);
+		userIds.add(2);
+		userIds.add(3);
+		user.setUserIdList(userIds);
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelMap save(String username, String password) {
+		User user = new User(username, password, "admin", new Date(), "admin", new Date());
+		user = userService.save(user);
+		System.out.println(user);
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, 0);
 	}
 
 }
