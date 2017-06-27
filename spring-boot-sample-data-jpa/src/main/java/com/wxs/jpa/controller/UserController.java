d3c22ab476e55acea681f1e5aa8737d6a00ec912
renfuji <renfuji@quarkfinance.com>
2017-06-27 17:19:37 +0800

    Spring-RestTemplate实战

--- spring-boot-sample-data-jpa/src/main/java/com/wxs/jpa/controller/UserController.java
+++ spring-boot-sample-data-jpa/src/main/java/com/wxs/jpa/controller/UserController.java
@@ -5,6 +5,7 @@ import com.wxs.jpa.services.UserService;
 import com.wxs.jpa.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,40 @@ public class UserController extends BaseResult {
 
 	@Autowired
 	private UserService userService;
+
+	@RequestMapping(value = "/selectGet/{id}", method = RequestMethod.GET)
+	public ModelMap selectGet(@PathVariable Integer id) {
+		User user = userService.selectOne(id);
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, user);
+	}
+
+	@RequestMapping(value = "/selectPost", method = RequestMethod.POST)
+	public ModelMap selectPost(String username, String password) {
+		List<User> userList = userService.selectPost(username, password);
+
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, userList);
+	}
+
+	@RequestMapping(value = "/selectPut", method = RequestMethod.PUT)
+	public ModelMap selectPut(String username, String password) {
+		List<User> userList = userService.selectPost(username, password);
+
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, userList);
+	}
+
+	@RequestMapping(value = "/selectDelete/{id}", method = RequestMethod.DELETE)
+	public ModelMap selectDelete(@PathVariable Integer id) {
+
+		userService.selectDelete(id);
+
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, 0);
+	}
+
+	@RequestMapping(value = "/selectHead/{id}", method = RequestMethod.HEAD)
+	public ModelMap selectHead(@PathVariable Integer id) {
+		User user = userService.selectOne(id);
+		return result(Constant.SUCCESS_CODE, Constant.SUCCESS_MSG, user);
+	}
 
 	@RequestMapping(value = "/search", method = RequestMethod.POST)
 	public ModelMap login() {
--- spring-boot-sample-data-jpa/src/test/java/com/wxs/jpa/user/UserServiceTest.java
+++ spring-boot-sample-data-jpa/src/test/java/com/wxs/jpa/user/UserServiceTest.java
@@ -1,11 +1,28 @@
 package com.wxs.jpa.user;
 
 import com.wxs.jpa.services.UserService;
+import com.wxs.jpa.vo.UserVo;
+import net.sf.json.JSONObject;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:
@@ -23,5 +40,99 @@ public class UserServiceTest {
 	public void selectAll() {
 		System.out.println("===========selectAll===============");
 		System.out.println(userService.selectAll());
+	}
+
+	@Test
+	public void selectPost1() {
+		//参数
+		String url = "http://localhost:9091/user/selectPost";
+		String username = "admin";
+		String password = "1212";
+
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("username", username);
+		map.add("password", password);
+		//设置响应头
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		//封装参数响应头
+		HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(map, headers);
+
+		//创建默认示例，初始化HttpMessageConverter
+		RestTemplate template = new RestTemplate();
+		//执行请求，并接受返回结果
+		ResponseEntity<String> result = template.exchange(url, HttpMethod.POST, entity, String.class);
+		JSONObject jsonObject = JSONObject.fromString(result.getBody());
+		System.out.println("===========selectPost1===============");
+
+		System.out.println(jsonObject);
+
+	}
+
+	@Test
+	public void selectPost2() {
+		//参数
+		String url = "http://localhost:9091/user/selectPost";
+		String username = "admin";
+		String password = "1212";
+
+		//封装参数
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("username", username);
+		map.add("password", password);
+
+		//设置响应头
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		//封装参数到HttpEntity
+		HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(map, headers);
+
+		//创建默认示例，初始化HttpMessageConverter
+		RestTemplate template = new RestTemplate();
+
+		//执行请求并获取返回结果
+		UserVo result = template.postForObject(url, entity, UserVo.class);
+
+		System.out.println("===========selectPost2===============");
+
+		System.out.println(result);
+	}
+
+	@Test
+	public void selectPost3() {
+		//参数
+		String url = "http://localhost:9091/user/selectPost";
+		String username = "admin";
+		String password = "1212";
+
+		try {
+			//初始化
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			CloseableHttpResponse httpResponse = null;
+			HttpPost httpPost = new HttpPost(url);
+
+			//请求参数
+			List<NameValuePair> nameValuePairs = new ArrayList<>();
+			nameValuePairs.add(new BasicNameValuePair("username", username));
+			nameValuePairs.add(new BasicNameValuePair("password", password));
+
+			//编码 封装参数
+			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(nameValuePairs, "utf-8");
+			httpPost.setEntity(formEntity);
+			httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+			//执行请求
+			httpResponse = httpClient.execute(httpPost);
+
+			//返回结果
+			org.apache.http.HttpEntity respHttpEntity = httpResponse.getEntity();
+			String resultStr = EntityUtils.toString(respHttpEntity, "utf-8");
+
+			System.out.println(resultStr);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
