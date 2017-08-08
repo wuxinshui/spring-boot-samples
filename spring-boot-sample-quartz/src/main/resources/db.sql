08a5a53b3a0f49a8c12456018b86c772c0569ce3
renfuji <renfuji@quarkfinance.com>
2017-08-08 18:34:04 +0800

    add t_job_info

--- spring-boot-sample-quartz/src/main/resources/db.sql
+++ spring-boot-sample-quartz/src/main/resources/db.sql
@@ -1,33 +1,28 @@
 -- ----------------------------
--- Table structure for t_scheduling
+-- Table structure for t_job_info
 -- ----------------------------
-DROP TABLE IF EXISTS t_scheduling;
-CREATE TABLE t_scheduling (
+DROP TABLE IF EXISTS t_job_info;
+CREATE TABLE t_job_info (
   id int(11) NOT NULL,
-  job varchar(255) DEFAULT NULL COMMENT 'job名称',
-  task_class varchar(255) DEFAULT NULL,
-  execute_class varchar(255) DEFAULT NULL COMMENT '执行类',
-  trigger_cron varchar(255) DEFAULT NULL COMMENT 'cron表达式',
-  status varchar(20) DEFAULT NULL COMMENT '当前job状态',
-  create_user varchar(100) DEFAULT NULL COMMENT '创建者',
+  job_name varchar(255) DEFAULT NULL COMMENT '任务名称',
+  job_group varchar(255) DEFAULT NULL COMMENT '任务分组',
+  job_description varchar(255) DEFAULT NULL COMMENT '任务描述',
+  job_status varchar(255) DEFAULT NULL COMMENT '任务状态',
+  job_class varchar(255) DEFAULT NULL COMMENT '任务类',
+  trigger_name varchar(255) DEFAULT NULL COMMENT '触发器名称',
+  trigger_group varchar(255) DEFAULT NULL COMMENT '触发器分组',
+  trigger_status varchar(255) DEFAULT NULL COMMENT '触发器状态',
+  trigger_description varchar(255) DEFAULT NULL COMMENT '触发器描述',
+  cron_expression varchar(255) DEFAULT NULL COMMENT 'Cron表达式',
+  create_user varchar(100) DEFAULT NULL COMMENT '创建人',
   create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   update_user varchar(100) DEFAULT NULL COMMENT '修改者',
-  update_time timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 -- ----------------------------
--- Table structure for t_scheduling_his
+-- Records of t_job_info
 -- ----------------------------
-DROP TABLE IF EXISTS t_scheduling_his;
-CREATE TABLE t_scheduling_his (
-  id int(11) NOT NULL,
-  job_id varchar(255) DEFAULT NULL COMMENT 'jobID',
-  execute_time timestamp NULL DEFAULT NULL COMMENT 'cron表达式',
-  create_user varchar(100) DEFAULT NULL COMMENT '创建者',
-  create_time timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  update_user varchar(100) DEFAULT NULL COMMENT '修改者',
-  update_time timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO t_job_info VALUES ('1', 'Job1', 'JobGroup1', 'JobGroup.Job1', 'GOING', 'com.wxs.quartz.task.Job1', 'trigger1', 'TriggerGroup1', 'GOING', 'TriggerGroup.trigger1', '0/1 * * * * ?', null, '2017-08-08 15:25:10', null, '2017-08-08 15:25:10');
+INSERT INTO t_job_info VALUES ('2', 'Job2', 'JobGroup2', 'JobGroup.Job2', 'GOING', 'com.wxs.quartz.task.Job2', 'trigger2', 'TriggerGroup2', 'GOING', 'TriggerGroup.trigger1', '0/1 * * * * ?', '', '2017-08-08 15:25:10', '', '2017-08-08 15:25:10');
