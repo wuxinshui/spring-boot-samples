fa05007573e251a3cc64129febddcd91bda29868
renfuji <renfuji@quarkfinance.com>
2017-08-09 16:23:37 +0800

    spring-boot-sample-redis init:

--- spring-boot-sample-quartz/src/main/java/com/wxs/quartz/mapper/JobInfoMapper.java
+++ spring-boot-sample-quartz/src/main/java/com/wxs/quartz/mapper/JobInfoMapper.java
@@ -2,9 +2,10 @@ package com.wxs.quartz.mapper;
 
 import com.wxs.quartz.model.JobInfo;
 import com.wxs.quartz.util.TKMapper;
+import org.springframework.data.repository.query.Param;
 
 public interface JobInfoMapper extends TKMapper<JobInfo> {
-	void updateJobByJobKey(String jobGroup,String jobName,String jobStatus);
+	void updateJobByJobKey(@Param("jobGroup") String jobGroup, @Param("jobName") String jobName, @Param("jobStatus") String jobStatus);
 
-	JobInfo selectJobByJobKey(String jobGroup,String jobName);
+	JobInfo selectJobByJobKey(@Param("jobGroup") String jobGroup, @Param("jobName") String jobName);
 }
