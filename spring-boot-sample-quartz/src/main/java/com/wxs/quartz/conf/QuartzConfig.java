49e4c995797bb9332dded6d92c2bede11b074018
renfuji <renfuji@quarkfinance.com>
2017-08-10 13:59:59 +0800

    init schedule no job 		SchedulerFactory sf = new StdSchedulerFactory(); 		Scheduler scheduler = sf.getScheduler(); 		scheduler.start(); 		return scheduler;

--- spring-boot-sample-quartz/src/main/java/com/wxs/quartz/conf/QuartzConfig.java
+++ spring-boot-sample-quartz/src/main/java/com/wxs/quartz/conf/QuartzConfig.java
@@ -1,15 +1,11 @@
 package com.wxs.quartz.conf;
 
-import com.wxs.quartz.job.InitJob;
-import org.quartz.*;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-
-import java.text.ParseException;
 
 /**
  * @ClassName: JobConfig
@@ -24,46 +20,55 @@ import java.text.ParseException;
 @Configuration
 public class QuartzConfig {
 
-    @Bean(name = "scheduler")
-    public Scheduler scheduler(CronTrigger cronTrigger) throws SchedulerException {
+	@Bean(name = "scheduler")
+	public Scheduler scheduler() throws SchedulerException {
 
-        //SchedulerFactory sf = new StdSchedulerFactory();
-        //Scheduler scheduler = sf.getScheduler();
+		SchedulerFactory sf = new StdSchedulerFactory();
+		Scheduler scheduler = sf.getScheduler();
+		scheduler.start();
+		return scheduler;
+	}
 
-        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
-        factoryBean.setSchedulerFactoryClass(StdSchedulerFactory.class);
-        factoryBean.setTriggers(cronTrigger);
-        Scheduler scheduler = null;
-        try {
-            factoryBean.afterPropertiesSet();
-            scheduler = factoryBean.getScheduler();
-            scheduler.startDelayed(60);
-            //scheduler.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return scheduler;
-    }
-
-    @Bean(name = "cronTrigger")
-    public CronTrigger cronTrigger(JobDetail jobDetail) throws ParseException {
-        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
-        cronTriggerFactoryBean.setJobDetail(jobDetail);
-        cronTriggerFactoryBean.setName("InitJobTrigger");
-        cronTriggerFactoryBean.setGroup("Default");
-        cronTriggerFactoryBean.setCronExpression("0/1 * * * * ?");
-        cronTriggerFactoryBean.afterPropertiesSet();
-        CronTrigger cronTrigger = cronTriggerFactoryBean.getObject();
-        return cronTrigger;
-    }
-
-    @Bean(name = "jobDetail")
-    public JobDetail jobDetail() {
-        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
-        jobDetailFactoryBean.setGroup("Default");
-        jobDetailFactoryBean.setName("InitJob");
-        jobDetailFactoryBean.setJobClass(InitJob.class);
-        jobDetailFactoryBean.afterPropertiesSet();
-        return jobDetailFactoryBean.getObject();
-    }
+	//@Bean(name = "scheduler")
+	//public Scheduler scheduler(CronTrigger cronTrigger) throws SchedulerException {
+	//
+	//    SchedulerFactory sf = new StdSchedulerFactory();
+	//    Scheduler scheduler = sf.getScheduler();
+	//
+	//    SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
+	//    factoryBean.setSchedulerFactoryClass(StdSchedulerFactory.class);
+	//    factoryBean.setTriggers(cronTrigger);
+	//    Scheduler scheduler = null;
+	//    try {
+	//        factoryBean.afterPropertiesSet();
+	//        scheduler = factoryBean.getScheduler();
+	//        //scheduler.startDelayed(60);
+	//        scheduler.start();
+	//    } catch (Exception e) {
+	//        e.printStackTrace();
+	//    }
+	//    return scheduler;
+	//}
+	//
+	//@Bean(name = "cronTrigger")
+	//public CronTrigger cronTrigger(JobDetail jobDetail) throws ParseException {
+	//    CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+	//    cronTriggerFactoryBean.setJobDetail(jobDetail);
+	//    cronTriggerFactoryBean.setName("InitJobTrigger");
+	//    cronTriggerFactoryBean.setGroup("Default");
+	//    cronTriggerFactoryBean.setCronExpression("0/1 * * * * ?");
+	//    cronTriggerFactoryBean.afterPropertiesSet();
+	//    CronTrigger cronTrigger = cronTriggerFactoryBean.getObject();
+	//    return cronTrigger;
+	//}
+	//
+	//@Bean(name = "jobDetail")
+	//public JobDetail jobDetail() {
+	//    JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+	//    jobDetailFactoryBean.setGroup("Default");
+	//    jobDetailFactoryBean.setName("InitJob");
+	//    jobDetailFactoryBean.setJobClass(InitJob.class);
+	//    jobDetailFactoryBean.afterPropertiesSet();
+	//    return jobDetailFactoryBean.getObject();
+	//}
 }
