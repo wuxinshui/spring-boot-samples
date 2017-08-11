adddddeec70cb08f30d148542f84b65db41ea800
renfuji <renfuji@quarkfinance.com>
2017-08-11 15:50:27 +0800

    addJob

--- spring-boot-sample-quartz/src/main/java/com/wxs/quartz/mapper/JobInfoMapper.java
+++ spring-boot-sample-quartz/src/main/java/com/wxs/quartz/mapper/JobInfoMapper.java
@@ -2,7 +2,7 @@ package com.wxs.quartz.mapper;
 
 import com.wxs.quartz.model.JobInfo;
 import com.wxs.quartz.util.TKMapper;
-import org.springframework.data.repository.query.Param;
+import org.apache.ibatis.annotations.Param;
 
 public interface JobInfoMapper extends TKMapper<JobInfo> {
 	void updateJobByJobKey(@Param("jobGroup") String jobGroup, @Param("jobName") String jobName, @Param("jobStatus") String jobStatus);
