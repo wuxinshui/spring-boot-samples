c5ef8cae29356c04601f37892652c5ecd8f2052e
FujiRen <FujiRen@quarkfinance.com>
2018-04-06 22:58:16 +0800

    跨局域网访问

--- spring-boot-sample-amqp/src/main/java/com/wxs/amqp/conf/AmqpInitConfig.java
+++ spring-boot-sample-amqp/src/main/java/com/wxs/amqp/conf/AmqpInitConfig.java
@@ -5,12 +5,18 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.SimpleRoutingConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,6 +30,9 @@ import org.springframework.context.annotation.Configuration;
 public class AmqpInitConfig {
 
     final static String queueName="spring.boot";
+
+    @Autowired
+    private MQConfig mqConfig;
 
     @Bean
     public Queue queue(){
@@ -41,9 +50,55 @@ public class AmqpInitConfig {
     }
 
     @Bean
-    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
+    public ConnectionFactory connectionFactory1(){
+        com.rabbitmq.client.ConnectionFactory rabbitConnectionFactory=new com.rabbitmq.client.ConnectionFactory();
+        rabbitConnectionFactory.setHost(mqConfig.getHost());
+        rabbitConnectionFactory.setPort(mqConfig.getPort());
+        rabbitConnectionFactory.setUsername(mqConfig.getUsername());
+        rabbitConnectionFactory.setPassword(mqConfig.getPassword());
+        rabbitConnectionFactory.setVirtualHost(mqConfig.getVirtualHost1());
+
+        CachingConnectionFactory factory = new CachingConnectionFactory(rabbitConnectionFactory);
+        return factory;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory2(){
+        com.rabbitmq.client.ConnectionFactory rabbitConnectionFactory=new com.rabbitmq.client.ConnectionFactory();
+        rabbitConnectionFactory.setHost(mqConfig.getHost());
+        rabbitConnectionFactory.setPort(mqConfig.getPort());
+        rabbitConnectionFactory.setUsername(mqConfig.getUsername());
+        rabbitConnectionFactory.setPassword(mqConfig.getPassword());
+        rabbitConnectionFactory.setVirtualHost(mqConfig.getVirtualHost1());
+
+        CachingConnectionFactory factory = new CachingConnectionFactory(rabbitConnectionFactory);
+        return factory;
+    }
+
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        SimpleRoutingConnectionFactory factory = new SimpleRoutingConnectionFactory();
+        Map<Object, ConnectionFactory> targetConnectionFactories = new HashMap<>();
+        targetConnectionFactories.put("connectionFactory1", connectionFactory1());
+        targetConnectionFactories.put("connectionFactory2", connectionFactory2());
+        factory.setTargetConnectionFactories(targetConnectionFactories);
+        return factory;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory1, MessageListenerAdapter listenerAdapter){
         SimpleMessageListenerContainer container=new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
+        container.setConnectionFactory(connectionFactory1);
+        container.setMessageListener(listenerAdapter);
+        container.addQueueNames(queueName);
+        return container;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container2(ConnectionFactory connectionFactory2, MessageListenerAdapter listenerAdapter){
+        SimpleMessageListenerContainer container=new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory2);
         container.setMessageListener(listenerAdapter);
         container.addQueueNames(queueName);
         return container;
---
+++ spring-boot-sample-amqp/src/main/java/com/wxs/amqp/conf/MQConfig.java
@@ -0,0 +1,69 @@
+package com.wxs.amqp.conf;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: FujiRen
+ * Date: 2018/4/6
+ * Time: 22:19
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "spring.rabbitmq",name = "enable", matchIfMissing = false)
+public class MQConfig {
+    private String username;
+    private String password;
+    private String virtualHost1;
+    private String virtualHost2;
+    private String host;
+    private int port;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVirtualHost1() {
+        return virtualHost1;
+    }
+
+    public void setVirtualHost1(String virtualHost1) {
+        this.virtualHost1 = virtualHost1;
+    }
+
+    public String getVirtualHost2() {
+        return virtualHost2;
+    }
+
+    public void setVirtualHost2(String virtualHost2) {
+        this.virtualHost2 = virtualHost2;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
