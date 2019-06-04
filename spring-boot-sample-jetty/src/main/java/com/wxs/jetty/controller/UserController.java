249dae41ce38deab0ee5c5a13462859aa5bb8e9c
任富吉 <fuji.ren@bkjk.com>
2019-06-04 16:52:14 +0800

    add pojo

--- spring-boot-sample-jetty/src/main/java/com/wxs/jetty/controller/UserController.java
+++ spring-boot-sample-jetty/src/main/java/com/wxs/jetty/controller/UserController.java
@@ -1,16 +1,33 @@
 package com.wxs.jetty.controller;
 
+import com.wxs.jetty.controller.vo.UserVo;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/jetty")
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/jetty")
     public String usersFromHeader() {
         return "hello jetty";
     }
+
+
+    @GetMapping("/user")
+    public ModelMap getUser() {
+        ModelMap modelMap = new ModelMap();
+        UserVo userVo = new UserVo();
+        userVo.setAge("12");
+        userVo.setName("12");
+        userVo.setSex("12");
+        modelMap.put("code", 200);
+        modelMap.put("msg", "success");
+        modelMap.put("data", userVo);
+        return modelMap;
+    }
+
+
 
 }
