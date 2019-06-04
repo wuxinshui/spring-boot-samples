154ea0e7b14dd8d9bd7698f58c77601f0a5421c9
任富吉 <fuji.ren@bkjk.com>
2019-06-04 16:51:32 +0800

    add pojo

--- spring-boot-sample-undertow/src/main/java/com/wxs/undertow/controller/UserController.java
+++ spring-boot-sample-undertow/src/main/java/com/wxs/undertow/controller/UserController.java
@@ -1,16 +1,31 @@
 package com.wxs.undertow.controller;
 
+import com.wxs.undertow.vo.UserVo;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/undertow")
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/undertow")
     public String usersFromHeader() {
         return "hello undertow";
     }
+
+
+    @GetMapping("/user")
+    public ModelMap getUser() {
+        ModelMap modelMap = new ModelMap();
+        UserVo userVo = new UserVo();
+        userVo.setAge("12");
+        userVo.setName("12");
+        userVo.setSex("12");
+        modelMap.put("code", 200);
+        modelMap.put("msg", "success");
+        modelMap.put("data", userVo);
+        return modelMap;
+    }
+
 
 }
