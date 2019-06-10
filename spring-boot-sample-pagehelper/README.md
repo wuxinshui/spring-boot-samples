3f2be62655a99c757738606b33db4b173244a87d
任富吉 <fuji.ren@bkjk.com>
2019-06-10 19:39:54 +0800

    pagehelper init No mapper.xml file

---
+++ spring-boot-sample-pagehelper/README.md
@@ -0,0 +1,9 @@
+# spring-boot-sample-paginator
+## 工程所用技术点
+
+[参考](https://github.com/abel533/MyBatis-Spring-Boot)
+
+### 1. Spring Boot
+### 2. Mybatis
+### 3. PageHelper
+### 4. No mapper.xml
---
+++ spring-boot-sample-pagehelper/src/main/java/com/wxs/pagehelper/model/UserInfo.java
@@ -0,0 +1,19 @@
+package com.wxs.pagehelper.model;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
