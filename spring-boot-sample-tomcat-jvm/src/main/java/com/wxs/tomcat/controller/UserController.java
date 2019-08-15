30a834dabaf8048753b25f1389e5b8d6525d3961
任富吉 <fuji.ren@bkjk.com>
2019-08-15 17:53:25 +0800

    add tomcat jvm

---
+++ spring-boot-sample-tomcat-jvm/src/main/java/com/wxs/tomcat/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.wxs.tomcat.controller;
+
+import com.wxs.tomcat.controller.vo.UserVo;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * simple string
+ *
+ * @author DELL
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/tomcat")
+    public String hello() {
+        return "hello tomcat";
+    }
+
+    @GetMapping("/user")
+    public ModelMap getUser() {
+        ModelMap modelMap = new ModelMap();
+        UserVo userVo = new UserVo();
+        userVo.setAge("12");
+        userVo.setName("12");
+        userVo.setSex("12");
+        modelMap.put("code", 200);
+        modelMap.put("msg", "success");
+        modelMap.put("data", userVo);
+        return modelMap;
+    }
+
+}
