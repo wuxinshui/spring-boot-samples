7d1ae3b826aa7e297ac9273c042058236f1d44b4
任富吉 <fuji.ren@bkjk.com>
2019-09-12 11:49:54 +0800

    add jmh

---
+++ spring-boot-sample-guava/src/main/java/com/wxs/guava/dao/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.wxs.pagehelper.dao;
+
+import com.wxs.pagehelper.model.UserInfo;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+}
---
+++ spring-boot-sample-rule/src/main/java/com/wxs/guava/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wxs.guava.dao;
+
+import com.wxs.guava.model.User;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
