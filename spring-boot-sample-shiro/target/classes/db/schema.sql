c99b2464fb317522724fffa2420c60dfae0b0eb4
rfg2012@163.com <secuity2012>
2019-09-15 11:57:02 +0800

    upgrade 2。18

--- spring-boot-sample-shiro/target/classes/db/schema.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS user;
-CREATE TABLE user  (
-  ID int(11) NOT NULL AUTO_INCREMENT,
-  NAME varchar(255),
-  AGE int(11)
-);
