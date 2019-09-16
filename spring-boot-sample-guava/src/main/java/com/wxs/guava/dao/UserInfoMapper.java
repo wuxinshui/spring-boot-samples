6dd4e6f6f055537fe68c4875afb66b02139f03be
任富吉 <fuji.ren@bkjk.com>
2019-09-16 19:01:57 +0800

    u

--- spring-boot-sample-guava/src/main/java/com/wxs/guava/dao/UserInfoMapper.java
+++ spring-boot-sample-guava/src/main/java/com/wxs/guava/dao/UserInfoMapper.java
@@ -1,6 +1,6 @@
 package com.wxs.guava.dao;
 
-import com.wxs.pagehelper.model.UserInfo;
+import com.wxs.guava.model.UserInfo;
 import tk.mybatis.mapper.common.BaseMapper;
 
 public interface UserInfoMapper extends BaseMapper<UserInfo> {
