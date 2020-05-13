3e3dafdcc36ef372547bf500338b2b490bf46acb
yoyo <yoyo@yoyo.com>
2020-05-13 11:46:15 +0800

    add redis config

--- spring-boot-sample-cache/src/main/java/com/wxs/cache/controller/UserController.java
+++ spring-boot-sample-cache/src/main/java/com/wxs/cache/controller/UserController.java
@@ -2,9 +2,11 @@ package com.wxs.cache.controller;
 
 import com.wxs.cache.entity.User;
 import com.wxs.cache.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/cache")
 public class UserController {
@@ -13,6 +15,7 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User usersFromSession(@PathVariable("id") Integer id) {
+        log.info("usersFromSession...");
         return userService.selectById(id);
     }
 
