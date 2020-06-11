530103aebf8c029140962a43120adc168f239cd2
yoyo <yoyo@yoyo.com>
2020-06-11 16:46:06 +0800

    add

--- spring-boot-sample-resttemplate/spring-boot-sample-resttemplate-server/src/java/com/wxs/rest/server/controller/ServerController.java
+++ spring-boot-sample-resttemplate/spring-boot-sample-resttemplate-server/src/java/com/wxs/rest/server/controller/ServerController.java
@@ -1,6 +1,6 @@
 package com.wxs.rest.server.controller;
 
-import com.wxs.rest.server.bo.Stu;
+import com.wxs.rest.server.bo.StuInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,37 +14,32 @@ import java.util.Map;
 @RequestMapping("/server")
 public class ServerController {
 
+
     @GetMapping("/")
     public Map index() {
         log.info("server......");
-        return Map.of("age",1,"name",1);
+        return Map.of("code", 200, "msg", "OK", "data", Map.of("age", 1, "name", 1));
     }
 
 
     @GetMapping("/map")
     public Map map(@RequestParam("age")Integer age, @RequestParam("name") String name) {
         log.info("server......map,params age:{},name:{}",age,name);
-        return Map.of("age",age,"name",name);
+        return Map.of("code", 200, "msg", "OK", "data", Map.of("age", age, "name", name));
     }
 
     @PostMapping("/json")
-    public Map json(@RequestBody Stu stu) {
-        log.info("server......map,params stu:{}",stu);
-        return Map.of("age",stu.getAge(),"name",stu.getName());
+    public Map json(@RequestBody StuInfo stu) {
+        log.info("server......json,params stu:{}", stu);
+        return Map.of("code", 200, "msg", "OK", "data", Map.of("age", stu.getAge(), "name", stu.getName()));
     }
 
-    // @GetMapping("/map")
-    // public Map map(Map map) {
-    //     log.info("server......map:{}",map);
-    //     return map;
-    // }
+    @PostMapping("/form")
+    public Map form(StuInfo stu) {
+        log.info("server......form,params stu:{}", stu);
+        return Map.of("code", 200, "msg", "OK", "data", Map.of("age", stu.getAge(), "name", stu.getName()));
+    }
 
-
-    // @GetMapping("/uriVariables")
-    // public Map uriVariables(@RequestParam("age")Integer age, @RequestParam("name") String name) {
-    //     log.info("server......map,params age:{},name:{}",age,name);
-    //     return Map.of("age",age,"name",name);
-    // }
 
     @GetMapping("/uriVariables")
     public Map uriVariablesFalse(@RequestParam(value = "age",required = false)Integer age, @RequestParam(value = "name",required = false) String name) {
