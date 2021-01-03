19a30324adaa6d5cf8746dc7b5d6bbc41c2fe575
yoyo <yoyo@yo.com>
2021-01-03 22:50:48 +0800

    h2database add

--- spring-boot-sample-mybatis/src/main/resources/db.sql
+++ spring-boot-sample-mybatis/src/main/resources/db.sql
@@ -9,3 +9,8 @@ CREATE TABLE t_user (
   update_time timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+insert into t_user values (1,'test','1234','test',NOW(),'test',now());
+insert into t_user values (2,'test2','12342','test2',NOW(),'test2',now());
+
